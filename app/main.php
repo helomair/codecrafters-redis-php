331712ea5114d85c747862426dcad2a6019548c6
helomair <very860112@gmail.com>
2024-03-26 22:15:21 +0800

    pass stage 4

--- app/main.php
+++ app/main.php
@@ -4,26 +4,35 @@ error_reporting(E_ALL);
 
 echo "Logs from your program will appear here";
 
-$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+$originSocket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
 // Since the tester restarts your program quite often, setting SO_REUSEADDR
 // ensures that we don't run into 'Address already in use' errors
-socket_set_option($sock, SOL_SOCKET, SO_REUSEADDR, 1);
-socket_bind($sock, "localhost", 6379);
+socket_set_option($originSocket, SOL_SOCKET, SO_REUSEADDR, 1);
+socket_bind($originSocket, "localhost", 6379);
 
-socket_listen($sock, 5);
+socket_listen($originSocket, 5);
 
-$connSocket = socket_accept($sock); // Wait for first client
+$socketPool = [$originSocket];
 
-while ($inputStr = socket_read($connSocket, 1024)) {
-    $inputs = explode("\r\n", $inputStr);
-    foreach ($inputs as $input) {
-        if ($input !== "ping")
+while (true) {
+    $tmp_socketPool = $socketPool;
+    $write = NULL;
+    $except = NULL;
+
+    socket_select($tmp_socketPool, $write, $except, NULL);
+
+    foreach ($tmp_socketPool as $index => $socket) {
+        if ($socket == $originSocket) {
+            $socket = socket_accept($originSocket); // Get another socket resources.
+            $socketPool[] = $socket;
+        }
+
+        $inputStr = socket_read($socket, 1024);
+
+        if (empty ($inputStr))
             continue;
 
-        socket_write($connSocket, "+PONG\r\n");
+        $ret = socket_write($socket, "+PONG\r\n");
     }
 }
-
-// socket_close($sock);
-
