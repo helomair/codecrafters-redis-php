25d7c89539f3dca745dbdbeb56756699b6204464
helomair <very860112@gmail.com>
2024-03-26 22:59:57 +0800

    pass stage 5

--- app/main.php
+++ app/main.php
@@ -1,38 +1,60 @@
 <?php
 error_reporting(E_ALL);
 
-
 echo "Logs from your program will appear here";
 
-$originSocket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
-// Since the tester restarts your program quite often, setting SO_REUSEADDR
-// ensures that we don't run into 'Address already in use' errors
-socket_set_option($originSocket, SOL_SOCKET, SO_REUSEADDR, 1);
-socket_bind($originSocket, "localhost", 6379);
+if (($originSocket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
+    echo "socket_create() failed : " . socket_strerror(socket_last_error()) . PHP_EOL;
+    exit(1);
+}
 
-socket_listen($originSocket, 5);
+if (socket_set_option($originSocket, SOL_SOCKET, SO_REUSEADDR, 1) === false) {
+    echo "socket_set_option() failed : " . socket_strerror(socket_last_error()) . PHP_EOL;
+    exit(1);
+}
 
-$socketPool = [$originSocket];
+if (socket_bind($originSocket, "localhost", 6379) === false) {
+    echo "socket_bind() failed : " . socket_strerror(socket_last_error()) . PHP_EOL;
+    exit(1);
+}
+
+if (socket_listen($originSocket, 5) === false) {
+    echo "socket_listen() failed : " . socket_strerror(socket_last_error()) . PHP_EOL;
+    exit(1);
+}
+
+socket_set_nonblock($originSocket);
+
+$socketPool = [];
 
 while (true) {
-    $tmp_socketPool = $socketPool;
-    $write = NULL;
-    $except = NULL;
+    if ($newSocket = socket_accept($originSocket)) {
+        $socketPool[] = $newSocket;
+        socket_set_nonblock($newSocket);
+    }
 
-    socket_select($tmp_socketPool, $write, $except, NULL);
-
-    foreach ($tmp_socketPool as $index => $socket) {
-        if ($socket == $originSocket) {
-            $socket = socket_accept($originSocket); // Get another socket resources.
-            $socketPool[] = $socket;
-        }
-
+    foreach ($socketPool as $index => $socket) {
         $inputStr = socket_read($socket, 1024);
 
-        if (empty ($inputStr))
+        if (!$inputStr)
             continue;
 
-        $ret = socket_write($socket, "+PONG\r\n");
+        $params = [];
+        foreach (explode("\r\n", $inputStr) as $index => $str) {
+            if ($index === 0) continue;
+            if (strpos($str, '$') !== false) continue;
+
+            $params[] = $str;
+        }
+
+        $command = $params[0];
+        if ($command === "ping") {
+            socket_write($socket, "+PONG\r\n");
+        } else if ($command === "echo") {
+            $echoStr = $params[1];
+            $length = strlen($echoStr);
+            socket_write($socket, "$$length\r\n$echoStr\r\n");
+        }
     }
 }
