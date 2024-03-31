dcfc3b8171a15bf96ca708cfd2cabfbf88468c7f
helomair <very860112@gmail.com>
2024-04-01 01:09:17 +0800

    pass streams 8

--- app/Redis/Commands/XrangeCommand.php
+++ app/Redis/Commands/XrangeCommand.php
@@ -26,13 +26,16 @@ class XrangeCommand {
         foreach($streamData->getEntries() as $id => $values) {
             [$nowMs, $nowSeq] = explode('-', $id);
 
-            if ($startMs > $nowMs || $endMs < $nowMs) continue;
+            if ( ($startMs > $nowMs) || ($endID !== '0-0' && $endMs < $nowMs) )
+                continue;
 
             // Ms in range, check Seq.
             // start: a-10 vs now: a-0
-            if ($startMs === $nowMs && $startSeq > $nowSeq) continue;
+            if ($startMs === $nowMs && $startSeq > $nowSeq) 
+                continue;
             // end: a-0 vs now: a-10
-            if ($endMs === $nowMs && $endSeq < $nowSeq) continue;
+            if ( ($endID !== '0-0') && ($endMs === $nowMs) && ($endSeq < $nowSeq) ) 
+                continue;
             
             $flattenValues = [];
             foreach($values as $innerKey => $value) {
@@ -46,9 +49,9 @@ class XrangeCommand {
     }
 
     private static function makeActualID(string $id): string {
-        if ($id === '-')
+        if ($id === '-' || $id === '+')
             return '0-0';
-        
+
         if(strpos($id, '-') !== false)
             return $id;
 
